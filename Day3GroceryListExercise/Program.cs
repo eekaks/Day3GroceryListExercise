91006e71e348e489e0f0129873d9cfa4cb32fd4b
Eetu Laine <eetulaine@gmail.com>
2022-06-08 10:43:14 +0300

    first functioning commit of app

---
+++ Day3GroceryListExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3GroceryListExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
