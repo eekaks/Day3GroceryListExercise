f3265d80c8832ce0a0862725538f5e6cc0c91b1d
Eetu Laine <eetulaine@gmail.com>
2022-06-08 10:44:46 +0300

    first commit failed to add files, fixing

--- Day3GroceryListExercise/Program.cs
+++ Day3GroceryListExercise/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Day3GroceryListExercise
 {
@@ -6,7 +8,51 @@ namespace Day3GroceryListExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                string path = "groceries.txt";
+                
+                Console.WriteLine("This is your grocery list. What would you like to buy?\n0 to quit. 1 to print list. 'DELETE' to delete your grocery list.\nEnter your item:");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "0")
+                {
+                    break;
+                }
+                else if (userInput == "DELETE")
+                {
+                    File.Delete(path);
+                }
+                else if (userInput == "1")
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string line = String.Empty;
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+                else
+                {
+                    if (!File.Exists("groceries.txt"))
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(userInput);
+                        }
+                    else
+                    {
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.WriteLine(userInput);
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
         }
+        
     }
 }
